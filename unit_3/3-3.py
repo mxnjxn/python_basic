c08e7383769d82c88294ff247309f67d48576e34
mxnjxn <choe86898@gmail.com>
2022-04-13 15:18:25 +0900

    ..

---
+++ unit_3/3-3.py
@@ -0,0 +1,24 @@
+# 3-3 숫자 처리 함수
+from math import *
+
+print("3-3 숫자 처리 함수")
+
+# abs --> absolute value
+print(abs(-5))
+
+# pow() --> exponential function
+print(pow(4, 2))  # 4^2
+
+# max() --> compare the numbers and output the bigger number
+print(max(5, 12))
+
+# min()
+print(min(3, 2))
+
+# round()
+print(round(4.99))
+
+# Use modules in library math
+print(floor(4.99))  # 내림.
+print(ceil(3.14))  # 올림
+print(sqrt(16))  # 제곱근
